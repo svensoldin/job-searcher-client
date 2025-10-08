54bfad2ccf16bbf6aab37c544e87f06737eba8bd
Sven Soldin <soldinsven@gmail.com>
2025-10-08 16:37:01 +0200

    chore: fix import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
-import ThemeToggle from '@/app/components/ThemeToggle';
+import ThemeToggle from '@/components/ui/ThemeToggle';
 import './globals.css';
 
 const geistSans = Geist({
